f7a378e36457bdaac425aa96b8d4f940e99e0892
orcaproject42 <orcaproject42@gmail.com>
2013-01-27 23:04:35 -0500

    Revised Commit

--- products/d2att.mk
+++ products/d2att.mk
@@ -1,14 +1,13 @@
 # Inherit AOSP device configuration for d2att.
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
+# Inherit GSM common stuff.
+$(call inherit-product, vendor/orca/configs/gsm.mk)
+
 # Inherit common product files.
-$(call inherit-product, vendor/orca/config/common.mk)
+$(call inherit-product, vendor/orca/configs/common.mk)
 
-# Inherit CDMA common stuff
-$(call inherit-product, vendor/orca/config/cdma.mk)
-
-# d2att Overlay
-PRODUCT_PACKAGE_OVERLAYS += vendor/orca/overlay/toro
+PRODUCT_PACKAGE_OVERLAYS += vendor/orca/overlay/d2-common
 
 # Setup device specific product configuration.
 PRODUCT_NAME := orca_d2att
@@ -17,10 +16,8 @@ PRODUCT_DEVICE := d2att
 PRODUCT_MODEL := SGH-I747
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2att TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2att-user 4.1.2 JZO54K I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2att/d2att:4.1.2/JZO54K/I747UCALEM:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2att TARGET_DEVICE=d2att BUILD_FINGERPRINT="samsung/d2uc/d2att:4.0.4/IMM76D/I747UCALH9:user/release-keys" PRIVATE_BUILD_DESC="d2att-user 4.0.4 IMM76D I747UCALH9 release-keys"
 
-# Copy mako specific prebuilt files
-PRODUCT_COPY_FILES +=  \
-    vendor/orca/prebuilt/bootanimation/bootanimation.zip:system/media/bootanimation.zip \
-    vendor/orca/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/orca/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd 
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/orca/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation-alt.zip
