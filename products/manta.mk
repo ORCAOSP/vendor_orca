f5f1b12d713f37686974b1e51a61c6b5ae3d0e2a
Orca Project <orcaproject@yahoo.com>
2013-03-24 01:46:28 -0300

    build fix

--- products/manta.mk
+++ products/manta.mk
@@ -15,5 +15,3 @@ PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray BUILD_FINGERPRINT=google/mantaray/manta:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2.2 JDQ39 573038 release-keys" BUILD_NUMBER=573038
-
-change build prop
